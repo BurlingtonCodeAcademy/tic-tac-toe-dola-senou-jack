f1b19b18dcddbf21556c3264ba343d99c46a9084
Senou Lynn <senou.lynn@gmail.com>
2021-02-19 16:04:48 -0500

    headway baby

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,19 @@
-let targId ;
+let targId;
 let targ;
 let Xicon = `Xicon`;
 let Oicon = `Oicon`;
 let board = document.getElementById("board");
-let count =0;
+let count = 0;
+let cells = document.getElementsByClassName("cell")
+
+let startBut = document.getElementById("start");
+let resetBut = document.getElementById("reset");
 
 let currentPlayer = [];
 let playerSwitch = true;
 let curIcon;
 let playerOneMoves = [];
 let playerTwoMoves = [];
-
-// function to switch turns
-// function to change player icon
-function playerTurn() {
-  if (playerSwitch === true) {
-    currentPlayer = playerOneMoves;
-    playerSwitch = !playerSwitch;
-    curIcon = Xicon;
-
-  } else if (playerSwitch !== true) {
-    currentPlayer = playerTwoMoves;
-    playerSwitch = !playerSwitch;
-    curIcon = Oicon;
-  }
-}
 
 // winning conditions
 let winS = {
@@ -40,40 +29,92 @@ let winS = {
   rdiagonal: ["cell-6", "cell-4", "cell-2"],
 };
 
-// target the clicked image and getting it's Id
-board.addEventListener("click", (elmt) => {
+console.log(board)
+function resetGame() {
+    for(let cell of cells) {
+        // cell.classList = "";
+        // curIcon = ""
+        cell.className = '';
   
-  
-  console.log(targId)
-  console.log(currentPlayer.includes(targId))
-  
-  console.log(`player1 array ${playerOneMoves}`);
-  console.log(`player2 array ${playerTwoMoves}`);
-  console.log(`player array ${currentPlayer}`);
-  if (!playerOneMoves.includes(targId) && !playerTwoMoves.includes(targId)){
-    currentPlayer.push(targId);
-    targId = elmt.target.id;
+    }
+    playerOneMoves = [];
+    playerTwoMoves = [];
+    currentPlayer = [];
+    
+    startBut.disabled = false;
+    resetBut.disabled = true;
+    console.log(cells)
+}
+
+//Begin Game
+startBut.addEventListener("click", (startFunction) => {
+
+    startBut.disabled = true;
+    resetBut.disabled = false;
    
-    playerTurn();
-
-   
-    console.log(`player1 array ${playerOneMoves}`);
-    console.log(`player2 array ${playerTwoMoves}`);
-    console.log(`player array ${currentPlayer}`);
-    targ = document.getElementById(targId);
-
-    //   console.log(targ);
-
-    targ.classList = curIcon
-
-    Object.keys(winS).forEach((win) => {
-      if (
-        currentPlayer.includes(winS[win][0]) &&
-        currentPlayer.includes(winS[win][1]) &&
-        currentPlayer.includes(winS[win][2])
-      ) {
-        console.log(`you won!`);
-      }
-    });
+  // function to switch turns
+  // function to change player icon
+  function playerTurn(evt) {
+    if (playerSwitch === true) {
+      currentPlayer = playerOneMoves;
+      curIcon = Xicon;
+    } else if (playerSwitch !== true) {
+      currentPlayer = playerTwoMoves;
+      curIcon = Oicon;
+    }
   }
+
+  // target the clicked image and getting it's Id
+  function gamePlay (elmt) {
+    console.log(targId);
+
+    //   console.log(currentPlayer.includes(targId));
+
+    //   console.log(`player1 array ${playerOneMoves}`);
+    //   console.log(`player2 array ${playerTwoMoves}`);
+    //   console.log(`player array ${currentPlayer}`);
+
+    playerTurn();
+    //Grab parent element of clicked target
+    targId = elmt.target.id;
+    //Guard clause against previously plays
+    if (
+      !playerOneMoves.includes(targId) &&
+      !playerTwoMoves.includes(targId) &&
+      targId !== "board"
+    ) {
+      currentPlayer.push(targId);
+      console.log(targId);
+    
+    //Switch Player
+      playerSwitch = !playerSwitch;
+
+
+      console.log(`player1 array ${playerOneMoves}`);
+      console.log(`player2 array ${playerTwoMoves}`);
+      console.log(`player array ${currentPlayer}`);
+      //Inserting correct picture based off current player
+      targ = document.getElementById(targId);
+      targ.classList = curIcon;
+    //Check against Win Scenarios
+      Object.keys(winS).forEach((win) => {
+        if (
+          currentPlayer.includes(winS[win][0]) &&
+          currentPlayer.includes(winS[win][1]) &&
+          currentPlayer.includes(winS[win][2])
+        ) {
+          console.log(`you won!`);
+
+          //Enter win state
+          //Currentplayer score ++
+        }
+      });
+    }
+  };
+  board.addEventListener("click", gamePlay)
+
+  resetBut.addEventListener("click", resetGame)
+  
+  
 });
+
